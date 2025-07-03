95f29c905dde18009b0e1678d997a940aed4a156
lahsiva-superfd <vishal@superfd.in>
2025-07-03 21:55:44 +0530

    Created server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express=require('express');
+const app=express(); // instance of express
+
+app.use((req,res)=>{
+    res.send('Hello from the server!');
+}); // server is responding to the request
+
+app.listen(3000,()=>{
+    console.log('Listening to port 3000...')
+}); // listening to port 3000 [Server]
