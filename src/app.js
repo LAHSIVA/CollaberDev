fe58cd56d64a822226ec353b92caf6e514a80a9f
lahsiva-superfd <vishal@superfd.in>
2025-07-04 20:03:03 +0530

    Tried Get,post,delete api call

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express=require('express');
 const app=express(); // instance of express
 
+//This will only handle GET call to /user
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Vishal",lastName:"V"});
+})
+
+//This will only handle POST call to /user
+app.post("/user",(req,res)=>{
+    console.log("Saving Data to the database");
+    res.send("Data Successfully saved to the database");
+})
+
+//This will only handle DELETE call to /user
+app.delete("/user",(req,res)=>{
+    console.log("Deleting Data from the database");
+    res.send("Data Successfully deleted from the database");
+})
+
+// This will match all the HTTP method API calls to /test
 app.use("/test",(req,res)=>{
     res.send('Hello from the server!');
 }); // server is responding to the request
