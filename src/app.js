d834da3928bfc0fde2a07406df3fb66f7f5ccdbf
lahsiva-superfd <vishal@superfd.in>
2025-07-07 23:35:32 +0530

    Added Passward hash using bcrypt

--- src/app.js
+++ src/app.js
@@ -2,13 +2,30 @@ const express=require('express');
 const connectDB=require("./config/database");
 const app = express(); // instance of express
 const User=require("./models/user");
-
+const{validateSignUpData}=require("./utils/validation");
+const bcrypt = require('bcrypt');
 app.use(express.json()); // middleware to parse json
 
 app.post("/signup",async (req, res) => {
-    // Creating new Instance of User Module
-    const user=new User(req.body);
     try{
+    // Validation of Data
+    validateSignUpData(req);
+
+    const{firstName,lastName,emailId,password}=req.body;
+
+    // Encrypt the password
+        const passwordHash=await bcrypt.hash(password,10);
+        console.log(passwordHash);
+
+
+    // Creating new Instance of User Module
+    const user=new User({
+        firstName,
+        lastName,
+        emailId,
+        password:passwordHash,
+    });
+    
         await user.save();
     res.send("User Added Successfully ");
     }catch(err){
