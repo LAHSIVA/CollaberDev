260e79cc3527b0df2c56ebdf68fe94c3f8ac907a
lahsiva-superfd <vishal@superfd.in>
2025-07-08 21:59:48 +0530

    Added userAuth Middleware set expiry for JWT Tokens and Cookies

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const{validateSignUpData}=require("./utils/validation");
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
 const jwt = require('jsonwebtoken');
+const {userAuth}=require('./middlewares/auth');
 
 app.use(express.json()); // middleware to parse json
 app.use(cookieParser());
@@ -48,11 +49,13 @@ app.post("/login",async(req,res)=>{
         if(ispasswordValid){
 
             // Create a JWT Token
-            const token=await jwt.sign({_id:user._id},"secret@2003");
+            const token=await jwt.sign({_id:user._id},"secret@2003",{expiresIn:"1d"});
             
 
             //Add the token to cookie and send the response back to user
-            res.cookie("token",token);
+            res.cookie("token",token,{
+                expires:new Date(Date.now()+8 *3600000),
+            });
 
             res.send("Login Successfull");
         }
@@ -65,22 +68,10 @@ app.post("/login",async(req,res)=>{
     }
 })
 
-app.get("/profile",async(req,res)=>{
+app.get("/profile",userAuth,async(req,res)=>{
     try{
-        const cookies = req.cookies;
-    const {token}=cookies;
-
-    if(!token){
-        throw new Error("Invalid Tokens");
-    }
-
-    const decodedMessage=await jwt.verify(token,"secret@2003");
-    const {_id}=decodedMessage;
-
-    const user=await User.findById(_id);
-    if(!user){
-        throw new Error("User does not exist");
-    }
+        
+    const user=req.user;
     res.send(user);
     }
     catch(err){
@@ -89,91 +80,14 @@ app.get("/profile",async(req,res)=>{
     
 });
 
-// Get user by email
-app.get("/user",async(req,res)=>{
-    const userEmail=req.body.emailId;
-    try{
-        const users=await User.find({emailId:userEmail});
-        if(users.length===0){
-            return res.status(404).send("User Not Found");
-        }
-        else{
-            res.send(users);
-        }
-    }
-    catch(err){
-        res.status(400).send("Something Went Wrong" + err.message);
-    }
-})
+app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
+    const user=req.user;
 
+    //Sending a connection request
+    console.log("Sending a connection request");
 
-// Feed API --> Get all users from the database
-app.get("/feed",async(req,res)=>{
-
-    try{
-        const users=await User.find({});
-        res.send(users);
-    }
-    
-    catch(err){
-        res.status(400).send("Something Went Wrong" + err.message);
-    }
-
-   
-    
-})
-
-// Delete the user from the Database
-app.delete("/user",async(req,res)=>{
-    const userId=req.body.userId;
-    try{
-        const users=await User.findByIdAndDelete(userId);
-        if(users.length===0){
-            return res.status(404).send("User Not Found");
-        }
-        else{
-            res.send("User Deleted Successfully");
-        }
-    }
-    catch(err){
-        res.status(400).send("Something Went Wrong" + err.message);
-    }
-})
-
-// Update data of the user
-app.patch("/user/:userId",async(req,res)=>{
-    const userId=req.params?.userId;
-    const data=req.body;
-
-    try{
-        const ALLOWED_UPDATES = [
-        "userId","skills","age","gender","photoUrl","about"
-    ];
-
-    const isUpdateAllowed = Object.keys(data).every((k) => 
-        ALLOWED_UPDATES.includes(k)
-    );
-    if(!isUpdateAllowed){
-         throw new Error("Update is not allowed");
-    }
-    if(data?.skills.length>10){
-        throw new Error("You cannot add more than 10 skills");
-        
-    }
-        const users=await User.findByIdAndUpdate({_id:userId},data);
-        if(users.length===0){
-            return res.status(404).send("User Not Found");
-        }
-        else{
-            res.send("User Updated Successfully");
-        }
-    }
-    catch(err){
-        res.status(400).send("Update Failed" + err.message);
-    }
-    
-})
-
+    res.send(user.firstName + "sent the connection request");
+});
 
 connectDB()
 .then(()=>{
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,12 +1,30 @@
-const adminAuth=(req,res,next)=>{
-    console.log("Admin Auth is getting checked!!");
-    const token="xyz";
-    const isAdminAuthorized=token ==="xyz";
-    if(!isAdminAuthorized){
-        return res.status(403).send("Unauthorized request");
-    }else{
+const jwt=require("jsonwebtoken");
+const User=require("../models/user");
+const userAuth= async(req,res,next)=>{
+       //Read the token from the cookies
+       try {
+       const {token}=req.cookies;
+       if(!token){
+        throw new Error("Token is Not valid");
+       }
+       const decodedObj=await jwt.verify(token,"secret@2003");
+
+        const {_id}=decodedObj;
+        const user=await User.findById(_id);
+        if(!user){
+            throw new Error("User Not Found");
+        }
+
+        req.user=user;
         next();
-    }    
+       } catch (error) {
+        res.status(400).send("ERROR" + error.message);
+       }
+       
+       //Validate the token
+       //Find the user
 };
 
-module.exports={adminAuth};
+module.exports={
+    userAuth,
+};
