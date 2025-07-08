a62611059216bebcdd062a2bcf2a15b6f42fc683
lahsiva-superfd <vishal@superfd.in>
2025-07-08 23:22:10 +0530

    Setup Express Router by grouping api's

--- src/app.js
+++ src/app.js
@@ -1,87 +1,18 @@
 const express=require('express');
 const connectDB=require("./config/database");
 const app = express(); // instance of express
-const User=require("./models/user");
-const{validateSignUpData}=require("./utils/validation");
-const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
-const jwt = require('jsonwebtoken');
-const {userAuth}=require('./middlewares/auth');
 
 app.use(express.json()); // middleware to parse json
 app.use(cookieParser());
 
-app.post("/signup",async (req, res) => {
-    try{
-    // Validation of Data
-    validateSignUpData(req);
+const authRouter=require("./routes/auth");
+const profileRouter=require("./routes/profile");
+const requestRouter=require("./routes/request");
 
-    const{firstName,lastName,emailId,password}=req.body;
-
-    // Encrypt the password
-        const passwordHash=await bcrypt.hash(password,10);
-        console.log(passwordHash);
-
-
-    // Creating new Instance of User Module
-    const user=new User({
-        firstName,
-        lastName,
-        emailId,
-        password:passwordHash,
-    });
-    
-        await user.save();
-    res.send("User Added Successfully ");
-    }catch(err){
-        res.status(400).send("Error saving the user" + err.message);
-    }
-});
-
-app.post("/login",async(req,res)=>{
-    try{
-        const {emailId,password}=req.body;
-        const user=await User.findOne({emailId:emailId});
-        if(!user){
-            throw new Error("Invalid Credentials");
-        }
-        const ispasswordValid=await user.validatePassword(password);
-        if(ispasswordValid){
-
-            
-            const token=await user.getJWT();
-            
-            res.cookie("token",token,{
-                expires:new Date(Date.now()+8 *3600000),
-            });
-
-            res.send("Login Successfull");
-        }
-        else{
-            throw new Error("Invalid Credentials");
-        }
-    }
-    catch(err){
-        res.status(400).send("Something Went Wrong" + err.message);
-    }
-})
-
-app.get("/profile",userAuth,async(req,res)=>{
-    try{     
-    const user=req.user;
-    res.send(user);
-    }
-    catch(err){
-        res.status(400).send("Something Went Wrong" + err.message);
-    }
-});
-
-app.post("/sendConnectionRequest",userAuth,async(req,res)=>{
-    const user=req.user;
-    //Sending a connection request
-    console.log("Sending a connection request");
-    res.send(user.firstName + "sent the connection request");
-});
+app.use("/",authRouter);
+app.use("/",profileRouter);
+app.use("/",requestRouter);
 
 connectDB()
 .then(()=>{
---
+++ src/routes/auth.js
@@ -0,0 +1,64 @@
+const express=require("express");
+const{validateSignUpData}=require("../utils/validation");
+const authRouter=express.Router();
+const User=require("../models/user");
+const bcrypt = require('bcrypt');
+
+
+authRouter.post("/signup",async (req, res) => {
+    try{
+    // Validation of Data
+    validateSignUpData(req);
+
+    const{firstName,lastName,emailId,password}=req.body;
+
+    // Encrypt the password
+        const passwordHash=await bcrypt.hash(password,10);
+        console.log(passwordHash);
+
+
+    // Creating new Instance of User Module
+    const user=new User({
+        firstName,
+        lastName,
+        emailId,
+        password:passwordHash,
+    });
+    
+        await user.save();
+    res.send("User Added Successfully ");
+    }catch(err){
+        res.status(400).send("Error saving the user" + err.message);
+    }
+});
+
+authRouter.post("/login",async(req,res)=>{
+    try{
+        const {emailId,password}=req.body;
+        const user=await User.findOne({emailId:emailId});
+        if(!user){
+            throw new Error("Invalid Credentials");
+        }
+        const ispasswordValid=await user.validatePassword(password);
+        if(ispasswordValid){
+
+            
+            const token=await user.getJWT();
+            
+            res.cookie("token",token,{
+                expires:new Date(Date.now()+8 *3600000),
+            });
+
+            res.send("Login Successfull");
+        }
+        else{
+            throw new Error("Invalid Credentials");
+        }
+    }
+    catch(err){
+        res.status(400).send("Something Went Wrong" + err.message);
+    }
+});
+
+
+module.exports=authRouter;
